3c48b7faf3b2fd7e258a2da33cbd719e13ca6dd6
wei.yuan17 <yuan_lian@bentley.edu>
2020-10-30 17:09:51 -0400

    index.php from Products

--- tech_support/product_manager/index.php
+++ tech_support/product_manager/index.php
@@ -1,3 +1,4 @@
+<!-- This is Part A of Assignment 3: Manage Products (Wei) -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,23 +38,11 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Navbar Content -->
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav ml-auto">
-                <li class="navbar-nav navItem">
-                    <a class="nav-link link" href="./index.html"><span class="">Home</span></a>
-                </li>
-                <li class="navbar-nav navItem">
-                    <a class="nav-link link" href="./Assign1/index.html"><span class="">Assignment 1</span></a>
-                </li>
-                <li class="navbar-nav navItem">
-                    <a class="nav-link link" href="./person.php"><span class="">Person</span></a>
-                </li>
-                <li class="navbar-nav navItem">
-                    <a class="nav-link link" href="./census.php"><span class="">Census</span></a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+        <?php 
+        
+        require "../view/nav.php";
+        
+        ?>
 
 <!-- PAGE TITLE -->
     <div class='pageTitleContainer'>
@@ -81,6 +70,8 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<a href="addProduct.php">Add Product</a>
 
 </body>
 
