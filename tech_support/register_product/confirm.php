80eaeec747f338248cb00ddadaf5778d4df7e4c5
delfinotrev <delfino_trev@bentley.edu>
2020-11-06 18:31:47 -0500

    confirm.php file for register product

---
+++ tech_support/register_product/confirm.php
@@ -0,0 +1,53 @@
+<?php 
+
+require '../view/header.php';
+
+makeHeader('Register Index');
+
+?>
+
+<!-- RESPONSIVE NAVBAR -->
+<?php
+    require '../view/nav.php'
+?>
+
+<!-- PAGE TITLE -->
+<div class='pageTitleContainer'>
+    <div class='pageTitle'>
+        Register
+    </div>
+</div>
+
+<!-- PAGE CONTENT -->
+<div class='sectionContainer'>
+   <?php 
+   if (!empty($_GET['message'])) {
+       
+       $message = $_GET['message'];
+       
+       require "../errors/successMessage.php";
+       
+       successMessage($message);
+       
+   } 
+   
+   if (!empty($_GET['error'])) {
+       
+       $error = $_GET['error'];
+       
+       require "../errors/errorMessage.php";
+       
+       customErrorMessage($error);
+       
+   }
+   
+   //session_destroy();
+   
+   ?>
+</div>
+
+<?php 
+
+require '../view/footer.php';
+
+?>
