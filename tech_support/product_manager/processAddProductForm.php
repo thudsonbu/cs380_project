e9f218e7286235e3948bf7ff2c8e45fee5e95549
thudsonbu <hudson_thom@bentley.edu>
2020-11-20 23:03:26 -0500

    added html injection detection to processAddProductForm

--- tech_support/product_manager/processAddProductForm.php
+++ tech_support/product_manager/processAddProductForm.php
@@ -15,12 +15,18 @@ if (! empty($_POST['code']) or
     require ("../model/postHandler.php");
     
     // test name for HTML characters to avoid HTML Injection
-    require ("testInput.php");
+    require ("../model/testInput.php");
 
-    $code = test_input($code);
-    $name = test_input($name);
-    $version = test_input($version);
-    $releaseDate = test_input($releaseDate);
+    $codeHtmlInjection = testInput($code);
+    $nameHtmlInjection = testInput($name);
+    $versionHtmlInjection = testInput($version);
+    $releaseDateHtmlInjection = testInput($releaseDate);
+
+    if($codeHtmlInjection or $nameHtmlInjection or $versionHtmlInjection or $releaseDateHtmlInjection){
+        $err = 'HTML INJECTION DETECTED';
+        header("Location: index.php?error=$err");
+        exit();
+    }
 
     // Perform SQL query
     $query = "INSERT INTO products (productCode, name, version, releaseDate) VALUES('$code', '$name', '$version', '$releaseDate')";
@@ -30,17 +36,17 @@ if (! empty($_POST['code']) or
     if(!empty($outarray[1])) {
         $error = $outarray[1]->getMessage();
         header("Location: index.php?error=$error");
+        exit();
     }
     // No messages but no records affected
     else if(!$outarray[0]) {
         $rowTotal = $outarray[0];
-        header("Location: index.php?error='$rowTotal Rows updated' ");
+        header("Location: index.php?message='$rowTotal Rows updated' ");
+        exit();
     }
     else {
         $rowTotal = $outarray[0];
         header("Location: index.php?message='$rowTotal Rows updated' ");
+        exit();
     }
-        
-    $result = mysqli_query($con, $query);
-
 ?>
