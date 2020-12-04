edb756cfb88b0aeb661e23ee727fd39a81cbdcc2
thudsonbu <hudson_thom@bentley.edu>
2020-12-04 17:05:15 -0500

    Removed customer customer styles and fixed search styles on incident search form

--- tech_support/create_incident/index.php
+++ tech_support/create_incident/index.php
@@ -65,13 +65,11 @@ echo "
             <div class='sectionTitleContainer'>
                 <p>Search for customer with email.</p>
             </div>
-            <div class='inputContainer'>
-                <div class='input'>
+            <div class='searchBar'>
+                <div class='searchBarInput'>
                     <input class='inputBox' placeholder='Email' type='text' name='email' required>
                 </div>
-                <div class='input'>
                 <button type='submit' class='button blue'>Search</button>
-                </div>
             </div>
         </form>
     </div>";
--- tech_support/customer_manager/customerSearchForm.php
+++ tech_support/customer_manager/customerSearchForm.php
@@ -5,15 +5,18 @@ echo "
 <form action='index.php' method='get' class='searchForm'>
     <div class='sectionTitleContainer'>
         <div class='sectionTitle'>
-            Search by Customer Lastname
+            Search Customers
         </div>
+    </div>
+    <div class='sectionTitleContainer'>
+        <p>Search for customer by lastname.</p>
     </div>
 ";
 
 // add the search input bar with search and reset buttons
 echo "
-    <div class='customerSearchBar'>
-        <div class='customerSearchBarInput'>
+    <div class='searchBar'>
+        <div class='searchBarInput'>
             <input placeholder='Lastname' type='text' name='lastname'>
         </div>
         <button type='submit' class='button blue'>Search</button>
